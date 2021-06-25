be7b5e3898890d62bd57e19f9b1785798467734d
hrso <hrso@rootlink.co.kr>
2021-06-25 16:34:57 +0900

    Add D173

---
+++ src/Paiza/D173.kt
@@ -0,0 +1,8 @@
+
+//[2021 설날 문제] 단어 교체
+//공백 문자열
+fun main() {
+    val input = readLine()!!.split(" ").map { it.toString() }
+    println("${input[1]} ${input[0]}")
+}
+
---
+++ src/Paiza/python/D173.py
@@ -0,0 +1,3 @@
+s1,s2 = input().split()
+
+print(s2 +" "+s1)
