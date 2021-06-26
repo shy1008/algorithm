69e0e4abbc2760148a8c3b40aafac07b4a79954d
shy1008 <lokixa@naver.com>
2021-06-26 23:15:30 +0900

    Add D175

---
+++ src/Paiza/D175.kt
@@ -0,0 +1,8 @@
+package Paiza
+
+// 12진수
+ fun main() {
+    val test = readLine()!!
+
+    println(test.replace("A","11").replace("B","12"))
+}
---
+++ src/Paiza/python/D175.py
@@ -0,0 +1,3 @@
+s1 = input()
+
+print(s1.replace("A", "10").replace("B", "11"))
