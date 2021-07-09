5c2f90c9d95e0c9404184e45f2ad7c7dd93a98ad
shy1008 <lokixa@naver.com>
2021-07-10 00:30:24 +0900

    Add D086

---
+++ src/Paiza/D086.kt
@@ -0,0 +1,8 @@
+package Paiza
+
+fun main() {
+    val minTree = readLine()!!.toInt()
+    val min = minTree / 3
+
+    println(min * 15)
+}
---
+++ src/Paiza/python/D086.py
@@ -0,0 +1,6 @@
+
+s1 = int(input())
+
+num = s1 / 3
+
+print(num * 15,end=" ")
