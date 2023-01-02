8af936dce66138f364af6ad1e1913dd3fd019806
shy1008 <lokixa96@gmail.com>
2023-01-02 22:12:42 +0900

    백준 11654 아스키 코드

---
+++ 백준 11654 아스키 코드/Main.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char N = br.readLine().charAt(0);
+        int n = N;
+
+        System.out.println(n);
+    }
+
+}
